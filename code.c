b0dbe3d779276209d4c40b95a0e6e447b81fce8c
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-03-18 09:09:52 -0700

    fix

--- code.c
+++ code.c
@@ -5,7 +5,7 @@ int main()
     enable_servos();
     set_servo_position(1,1216); // this sets the servo position to 1216 while at servo 1  
     set_servo_position(1,649);
-    set_servo_position(1216);
+    set_servo_position(1,1216);
     set_servo_position(1,649);
     ao() // turns off all servos
 }
