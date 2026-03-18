cf9c895dee7100b10f19702c3b06029847f0c4bb
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-03-18 11:10:37 -0700

    more comments

--- code.c
+++ code.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    enable_servos();
-    set_servo_position(1,1216); // this sets the servo position to 1216 while at servo 1  
-    set_servo_position(1,649);
-    set_servo_position(1,1216);
-    set_servo_position(1,649);
+    enable_servos(); // turns on all servos
+    set_servo_position(1,1216); // this sets the servo position to 1216 while on servo 1  
+    set_servo_position(1,649); // this sets the servo position to 649 while on servo 1
+    set_servo_position(1,1216); // this sets the servo position to 1216 while on servo 1 
+    set_servo_position(1,649); // this sets the servo position to 649 while on servo 1 
     ao() // turns off all servos
 }
