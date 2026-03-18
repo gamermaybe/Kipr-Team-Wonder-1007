b8790cb1d0644735e4c1585a8054de5c6b519254
gamermaybe <249988761+gamermaybe@users.noreply.github.com>
2026-03-18 11:20:01 -0700

    adds another function and removes servo_extend from main (for now)

--- code.c
+++ code.c
@@ -1,7 +1,11 @@
 #include <kipr/wombat.h>
 
 int main() {
-    servo_extend();
+    
+}
+
+int claw_open_and_close() {
+    
 }
 
 int servo_extend()
@@ -9,8 +13,8 @@ int servo_extend()
     enable_servos(); // turns on all servos
     set_servo_position(1,1216); // this sets the servo position to 1216 while on servo 1  
     set_servo_position(1,649); // this sets the servo position to 649 while on servo 1
-    set_servo_position(1,1216); // this sets the servo position to 1216 while on servo 1 
-    set_servo_position(1,649); // this sets the servo position to 649 while on servo 1 
-    ao() // turns off all servos
+    set_servo_position(1,1216);  
+    set_servo_position(1,649); 
+    ao(); // turns off all servos
 }
 
